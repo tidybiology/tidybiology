0a008dce0847f449600425b2ca22b8eb6fb31b01
hirscheylab <matthew.hirschey@duke.edu>
2019-10-03 06:38:33 -0400

    Added 4 OMIM datasets

--- R/data.R
+++ R/data.R
@@ -32,7 +32,7 @@
 
 #' Mitocarta: an inventory of mammalian mitochondrial genes
 #'
-#' MitoCarta2.0 is an inventory of 1158 human and mouse genes encoding proteins with strong support of mitochondrial localization. To generate this inventory, we performed mass spectrometry of mitochondria isolated from fourteen tissues, assessed protein localization through large-scale GFP tagging/microscopy, and integrated these results with six other genome-scale datasets of mitochondrial localization, using a Bayesian approach. MitoCarta2.0, released 2015, is an update from the MitoCarta1.0 inventory published in Cell, 2008.
+#' MitoCarta2.0 is an inventory of 1158 human genes encoding proteins with strong support of mitochondrial localization. To generate this inventory, the Mootha lab at MGH performed mass spectrometry of mitochondria isolated from fourteen tissues, assessed protein localization through large-scale GFP tagging/microscopy, and integrated these results with six other genome-scale datasets of mitochondrial localization, using a Bayesian approach. MitoCarta2.0, released 2015, is an update from the MitoCarta1.0 inventory published in Cell, 2008.
 #'
 #' @usage data(mitocarta)
 #'
@@ -89,3 +89,104 @@
 #'
 #' @source \url{https://www.broadinstitute.org/scientific-community/science/programs/metabolic-disease-program/publications/mitocarta/mitocarta-in-0}
 "mitocarta"
+
+
+#' omim_mim2gene
+#'
+#' A dataset linking MIM numbers with NCBI Gene IDs, Ensembl Gene IDs, and HGNC Approved Gene Symbols. For more informaation, see: https://omim.org/help/faq.
+#'
+#' @usage data(omim_mim2gene)
+#'
+#' @docType data
+#'
+#' @format A data frame with 26,445 rows and 5 variables:
+#' \describe{
+#'   \item{number_mim_number}{Each OMIM entry is given a unique six-digit number.}
+#'   \item{mim_entry_type}{Gene indicates that the entry contains the description of a gene of known sequence and a phenotype. See other types at faq 1.3 at https://omim.org help faq for more types.}
+#'   \item{entrez_gene_id_ncbi}{Official Entrez gene ID from NCBI.}
+#'   \item{approved_gene_symbol_hgnc}{Official gene symbol from HGNC.}
+#'   \item{ensembl_gene_id_ensembl}{Official gene ID from Ensembl.}
+#' }
+#'
+#' @keywords datasets
+#'
+#' @source \url{https://www.omim.org/downloads/}
+"omim_mim2gene"
+
+
+#' omim_mimtitles
+#'
+#' A dataset of MIM numbers and perferred titles.
+#'
+#' @usage data(omim_mimtitles)
+#'
+#' @docType data
+#'
+#' @format A data frame with 26,458 rows and 5 variables:
+#' \describe{
+#'   \item{number_prefix}{An (*) indicates a gene. A(#) indicates that it is a descriptive entry, usually of a phenotype, and not a unique locus. A (+) indicates the description of a gene of known sequence and a phenotype. A (%) indicates a confirmed mendelian phenotype or phenotypic locus for which the underlying molecular basis is not known. No symbol generally indicates a description of a phenotype for which the mendelian basis, although suspected, has not been clearly established. A (^) means the entry no longer exists because it was removed from the database or moved to another entry as indicated.}
+#'   \item{mim_number}{Each OMIM entry is given a unique six-digit number.}
+#'   \item{preferred_title_symbol}{ach entry has a “Preferred title” with symbol.}
+#'   \item{alternative_title_s_symbol_s}{Synonymous names and symbols that have been used in the literature are included as “Alternative title(s)”.}
+#'   \item{included_title_s_symbols}{“Included titles” mark placement of information on a topic that does not warrant a separate entry (e.g., fusion genes).}
+#' }
+#'
+#' @keywords datasets
+#'
+#' @source \url{https://www.omim.org/downloads/}
+"omim_mimtitles"
+
+
+#' omim_genemap
+#'
+#' A dataset containing OMIM's Synopsis of the Human Gene Map, including additional information such as genomic coordinates and inheritance.
+#'
+#' @usage data(omim_genemap)
+#'
+#' @docType data
+#'
+#' @format A data frame with 17,053 rows and 14 variables:
+#' \describe{
+#'   \item{number_chromosome}{Chromosomal location from NCBI}
+#'   \item{genomic_position_start}{Genomic position start from NCBI; for phenotypes with mapping key of (2) or genes not found in NCBI data, genomic coordinates are derived from OMIM's cyto location mapped to genomic using UCSC table.}
+#'   \item{genomic_position_end}{Genomic position end from NCBI}
+#'   \item{cyto_location}{Computed cyto location from UCSC}
+#'   \item{computed_cyto_location}{Annotated cyto location from OMIM}
+#'   \item{mim_number}{Each OMIM entry is given a unique six-digit number.}
+#'   \item{gene_symbols}{Gene symbols from OMIM}
+#'   \item{gene_name}{Gene name from OMIM}
+#'   \item{approved_symbol}{Approved gene symbol from HGNC}
+#'   \item{entrez_gene_id}{Official NCBI Entrez gene ID}
+#'   \item{ensembl_gene_id}{Official Ensembl gene ID}
+#'   \item{comments}{Curated, miscellaneous comments}
+#'   \item{phenotypes}{Phenotype data from OMIM}
+#'   \item{mouse_gene_symbol_id{Mouse gene symbol and ID from MGI}
+#' }
+#'
+#' @keywords datasets
+#'
+#' @source \url{https://www.omim.org/downloads/}
+"omim_genemap"
+
+
+#' omim_morbidmap
+#'
+#' A dataset containing OMIM's Synopsis of the Human Gene Map (same as omim_genemap) sorted alphabetically by disorder.
+#'
+#' @usage data(omim_morbidmap)
+#'
+#' @docType data
+#'
+#' @format A data frame with 7,831 rows and 4 variables:
+#' \describe{
+#'   \item{number_phenotype}{Each Phenotype is followed by its MIM number, if different from that of the locus/gene, and then followed by its phenotype mapping key in parentheses.}
+#'   \item{gene_symbols}{Gene symbol(s) associated with disease}
+#'   \item{mim_number}{Each OMIM entry is given a unique six-digit number.}
+#'   \item{cyto_location}{Computed cyto location from UCSC}
+#'   }
+#'
+#' @keywords datasets
+#'
+#' @source \url{https://www.omim.org/downloads/}
+"omim_morbidmap"
+
